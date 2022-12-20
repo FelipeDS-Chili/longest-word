6fc6f3ae1e766d96d4c4efc9108dff5ec0f600d8
felipe <falberto.inostroza@gmail.com>
2022-12-20 13:03:12 +0000

    Small web app wrapper around the Game

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from longest_word.game import Game
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    game = Game()
+    return render_template('home.html', grid=game.grid)
